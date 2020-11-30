c6e81c1b7d267718ea700ac4185c95b4b50fd535
gqc954 <ivan.vogel@gmail.com>
2020-11-30 19:57:49 +0100

    init file update

--- SureTypeSC/__init__.py
+++ SureTypeSC/__init__.py
@@ -2,10 +2,16 @@ __version__ = "0.2.0"
 
 from .genome_library import basic,statistic
 
-from .MachineLearning import Trainer, SerializedTrainer, starting_procedure_GM12878, tm_routine, evaluate_metrics
+#from .MachineLearning import Trainer, SerializedTrainer, starting_procedure_GM12878, tm_routine, evaluate_metrics
+from .MachineLearning import Trainer, SerializedTrainer, starting_procedure_GM12878, tm_routine, evaluate_metrics,starting_procedure
+#from .MachineLearning import *
 from .classifier import loader
 
 from .DataLoader import Data
+#from .Config import load,save
+
+
+
 
 
 
